75b9b25601c8e05b1646dcf8189340162f5f8057
Vsumavane <vsumavane@outlook.com>
2025-03-02 02:21:48 +0530

    created FileGetter Component

---
+++ frontend/src/Components/Body/FileGetter.jsx
@@ -0,0 +1,22 @@
+import Button from "../Buttons/Button";
+
+function FileGetter(){
+    return(
+        <>
+        <div className="flex justify-center items-center ">
+            <div className="flex justify-between items-center w-md ring-1 h-lg rounded-4xl">
+                <p className="px-6 font-light text-sm pointer-events-none">
+                    Drop your file here
+                </p>
+                <Button className="mx-2 my-2 rounded-4xl max-h-11">
+                    Upload File
+                    </Button>
+            </div>
+            <p className="px-4 font-light text-sm"> OR </p>
+            <Button className="rounded-4xl max-h-11">Generate with AI </Button>
+        </div>
+        </>
+    )
+}
+
+export default FileGetter;
