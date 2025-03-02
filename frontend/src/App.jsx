60d9286498623129781b67b846be0dd540f7c83b
Vsumavane <vsumavane@outlook.com>
2025-03-02 23:43:14 +0530

    making it work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from './Components/NavBar/NavBar'
 import HeadMessage from './Components/Body/HeadMessage'
 import ReviewCarousel from './Components/Carousel/ReviewsCarousel'
 import FileGetter from './Components/Body/FileGetter'
-import Background from './Components/Body/backgroud'
+import Background from './Components/Body/Background'
 
 function App() {
   // const img = 'https://img.freepik.com/free-vector/illustration-share-icon_53876-5843.jpg?t=st=1740597330~exp=1740600930~hmac=7bdb0b9b6b8da2b633f5d89034c7e1f3891f54238eced4bc2143b84bf73c0121&w=1380';
