90796851b4321539c7ea2b8932cb837436b64083
Vsumavane <vsumavane@outlook.com>
2025-03-02 23:36:08 +0530

    bakground files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './Components/NavBar/NavBar'
 import HeadMessage from './Components/Body/HeadMessage'
 import ReviewCarousel from './Components/Carousel/ReviewsCarousel'
 import FileGetter from './Components/Body/FileGetter'
+import Background from './Components/Body/backgroud'
 
 function App() {
   // const img = 'https://img.freepik.com/free-vector/illustration-share-icon_53876-5843.jpg?t=st=1740597330~exp=1740600930~hmac=7bdb0b9b6b8da2b633f5d89034c7e1f3891f54238eced4bc2143b84bf73c0121&w=1380';
@@ -10,6 +11,7 @@ function App() {
     <>
     <NavBar/>
     <HeadMessage/>
+    <Background/>
     <FileGetter/>
     <ReviewCarousel/>
     </>
---
+++ frontend/src/Components/Body/Backgroud.jsx
@@ -0,0 +1,11 @@
+export default function Background() {
+    return (
+        <>
+        <div className="">
+            <div className="absolute overflow-hidden left-0 ml-[-700px] blur-[1000px] bg-brand-blue w-[870px] h-[870px] rounded-full  -z-10"/>
+            <div className="absolute overflow- right-0 mr-[-700px] blur-[1000px] bg-brand-blue w-[870px] h-[870px] rounded-full  -z-10"/>
+        </div>
+            
+        </>
+    )
+}
