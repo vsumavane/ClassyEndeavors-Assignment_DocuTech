d5fb13f665043ca958648e7a8b68001be1228bbf
Vsumavane <vsumavane@outlook.com>
2025-03-05 02:58:58 +0530

    Create signup.jsx

---
+++ frontend/src/Components/NavBar/signup.jsx
@@ -0,0 +1,9 @@
+export function SignUp(){
+    return (
+        <>
+        <p className="w-full text-center px-2 py-2 bg-brand-blue text-white font-inter font-medium text-[20px]">
+            No Sign up required
+        </p>
+        </>
+    )
+}
